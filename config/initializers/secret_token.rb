40dac2578bf350fd3e210ed412165a6fc9456168
Spencer Montgomery <smontgo5@gmail.com>
2013-05-19 15:36:00 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Twitsimple::Application.config.secret_key_base = 'ff1fd47286cf75b5356ff8ec276ebf64ab9a4d694e13c1aaff69190e5a720dc0ad41f1f5ce78cf26847f95c92a0c552e0dc154496d51421263a84ac050f801a9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Twitsimple::Application.config.session_store :cookie_store, key: '_twitsimple_session'
